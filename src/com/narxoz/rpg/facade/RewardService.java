26e5aae4fc9b4be599f1ef4d5f6fc1225094ed33
ZhalgasB <zhalgas.bissenbai@narxoz.kz>
2026-03-19 21:03:29 +0500

    Reward and Preparation Services complited

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,10 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "Error";
         }
-        return "TODO";
+        if (battleResult.getWinner().contains("Warrior"))return "500gold + Legendary sword";
+        else return "No items";
     }
 }
